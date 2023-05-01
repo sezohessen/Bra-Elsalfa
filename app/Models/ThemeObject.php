feedce0c88474b13d3c966f0faf181abc91f454b
eslamabdallah74 <eslamabdallah301@gmail.com>
2023-05-01 11:48:05 +0300

    add controller

---
+++ app/Models/ThemeObject.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ThemeObject extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'game_theme_id',
+    ];
+
+    public function theme()
+    {
+        return $this->belongsTo(GameTheme::class,'game_theme_id');
+    }
+    
+}
